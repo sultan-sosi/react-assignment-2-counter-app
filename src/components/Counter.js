b3bb6f5d8d6ab4b91e2f4ec63210751d60924d1f
sultan <sultan.btc@gmail.com>
2022-12-18 19:53:33 +0600

    changes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  const handel = () => {
+    if (count <= 4) {
+      setCount(count + 1);
+    } else {
+      setCount((count = 4));
+    }
+  };
+  const change = () => {
+    if (count >= 1) {
+      setCount(count - 1);
+    } else {
+      setCount((count = 0));
+    }
+  };
+  const zero = () => {
+    setCount(count * 0);
+  };
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count: {count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button onClick={handel} className="btn card__btn">
+            +
+          </button>
+          <button onClick={change} className="btn card__btn">
+            -
+          </button>
+          <button onClick={zero} className="btn card__btn">
+            0
+          </button>
         </div>
       </div>
     </div>
